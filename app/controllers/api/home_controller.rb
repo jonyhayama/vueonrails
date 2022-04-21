c0f9c121b670cd36f6f6efc23de49c36601f0b6d
Jony Hayama <jonathan.hayama@gmail.com>
2022-04-21 06:41:11 -0300

    🎨 Create API namespace

--- app/controllers/api/home_controller.rb
+++ app/controllers/api/home_controller.rb
@@ -1,4 +1,4 @@
-class HomeController < ApplicationController
+class Api::HomeController < ApplicationController
   def index
     render json: { welcome: "Yay! you're on rails!" }
   end
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,10 +5,11 @@ import { onMounted } from 'vue';
 onMounted(async () => {
   console.log('connecting with Rails...');
   try {
-    const response = await fetch('http://localhost:3100');
+    const response = await fetch('http://localhost:3100/api');
     const data = await response.json();
     console.log('🎉 connected', data);
-  } catch {
+  } catch (err) {
+    console.error(err);
     console.log('😞 no dice');
   }
 });

5a20c4e6b65a6c2d5c7694252c2abb556a206160
Jony Hayama <jonathan.hayama@gmail.com>
2022-04-21 06:00:02 -0300

    🔧 Connect FE with BE on dev env

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    render json: { welcome: "Yay! you're on rails!" }
+  end
+end 
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,17 @@
 <script setup>
 import TheWelcome from '@/components/TheWelcome.vue'
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+  console.log('connecting with Rails...');
+  try {
+    const response = await fetch('http://localhost:3100');
+    const data = await response.json();
+    console.log('🎉 connected', data);
+  } catch {
+    console.log('😞 no dice');
+  }
+});
 </script>
 
 <template>
